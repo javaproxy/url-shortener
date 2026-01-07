d56cf3976f20163b711975d71b4cded56dece122
javaProxy <javaasproxy@gmail.com>
2026-01-07 14:43:58 +0530

    @RestController annotation missed earlier

--- url-shortener/src/main/java/com/example/url_shortener/controller/UrlController.java
+++ url-shortener/src/main/java/com/example/url_shortener/controller/UrlController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UrlController {
 	private final Map<String, String> store = new HashMap<>();
 
